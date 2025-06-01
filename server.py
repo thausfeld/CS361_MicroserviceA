ddeaa818f135383eae5acd29c592d29bf3be3c58
Tommy Hausfeld <59375272+thausfeld@users.noreply.github.com>
2025-05-31 21:28:45 -0700

    Issue 1 Resolved
    Addressed missing comments concerns.

--- server.py
+++ server.py
@@ -4,13 +4,17 @@ import datetime
 import zmq
 import json
 
+# Establish the connection settings on the network
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+# Accept requests indefinetly until prompted to "shut down"
 while True:
     message = socket.recv().decode()
     print(f'Recieved client message: {message}')
+
+    # Determine if client wants a response or to shut down
     if len(message) > 0:
         if message == 'Q':
             print('Client has requested to close connection!')
@@ -18,12 +22,17 @@ while True:
         if message != 'MSG':
             socket.send_string('ERR')
 
+        # Generate a random number using time
         randInt = datetime.datetime.now()
         randInt = str(((randInt.second) * (randInt.microsecond)) % 100)
+
+        # Gather data from local json file for the client
         with open('encouragement.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
             friendlyMessage = data[randInt]
             file.close()
+
+        # Send the gathered data to the client
         print(f'Sending response: {friendlyMessage}')
         socket.send_string(friendlyMessage)
 
